639e7bb46a2b24e1dbf5c73104386080bb2986d0
Tokenizer <mostafafahimi73@gmail.com>
2023-06-02 20:59:52 +0330

    feat: added base design for admin panel table

--- lib/src/core/constants/general_constants.dart
+++ lib/src/core/constants/general_constants.dart
@@ -5,4 +5,5 @@ const Duration kShortAnimationDuration = Duration(milliseconds: 400);
 const int kDefaultFetchSize = 20;
 const double kDefaultAppbarHeight = 0.075;
 const int maxFileSize = 209715200;
-const String adminToken = 'wyzMjqDXeeaGWJVgdysutJ6C9E3MX11t38LD2K60';
+// const String adminToken = 'wyzMjqDXeeaGWJVgdysutJ6C9E3MX11t38LD2K60';
+const String adminToken = '1';
--- lib/src/presentation/home/widgets/admin_panel_widget.dart
+++ lib/src/presentation/home/widgets/admin_panel_widget.dart
@@ -1,3 +1,4 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -21,10 +22,10 @@ class AdminPanelWidget extends HookWidget {
     return ContainerWithTitleCustomWidget(
       title: context.l10n.admin_panel,
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 25),
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
         child: Center(
           child: isLoggedIn.value
-              ? const SizedBox()
+              ? _SubmittedWorksTableWidget()
               : _LoginWidget(
                   isLoggedIn: isLoggedIn,
                   tokenController: tokenController,
@@ -103,5 +104,135 @@ class _LoginWidget extends StatelessWidget {
       );
     }
     isLoggedIn.value = true;
+  }
+}
+
+class _SubmittedWorksTableWidget extends StatelessWidget {
+  _SubmittedWorksTableWidget({Key? key}) : super(key: key);
+
+  final sc = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.deviceHeightFactor(0.5),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: DataTable2(
+          columnSpacing: 12,
+          border: TableBorder.all(color: Colors.black54),
+          horizontalMargin: 12,
+          minWidth: 2000,
+          columns: const [
+            DataColumn2(
+              label: Text('Column A'),
+              fixedWidth: 300,
+            ),
+            DataColumn2(
+              label: Text('Column A'),
+              size: ColumnSize.L,
+              fixedWidth: 120,
+            ),
+            DataColumn2(
+              label: Text('Column A'),
+              fixedWidth: 300,
+            ),
+            DataColumn2(
+              label: Text('Column A'),
+              size: ColumnSize.L,
+              fixedWidth: 120,
+            ),
+            DataColumn2(
+              label: Text('Column A'),
+              size: ColumnSize.L,
+              fixedWidth: 120,
+            ),
+            DataColumn2(
+              label: Text('Column A'),
+              size: ColumnSize.L,
+              fixedWidth: 120,
+            ),
+            DataColumn2(
+              label: Text('Column A'),
+              size: ColumnSize.L,
+              fixedWidth: 120,
+            ),
+            DataColumn2(
+              label: Text('Column A'),
+              size: ColumnSize.L,
+              fixedWidth: 120,
+            ),
+            DataColumn(
+              label: Text('Column B'),
+            ),
+            DataColumn(
+              label: Text('Column C'),
+            ),
+            DataColumn(
+              label: Text('Column D'),
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Column NUMBERS'),
+              numeric: true,
+            ),
+          ],
+          rows: List<DataRow2>.generate(
+            20,
+            (index) => DataRow2(
+              specificRowHeight: 30,
+              cells: [
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('A' * (10 - index % 10))),
+                DataCell(Text('B' * (10 - (index + 5) % 10))),
+                DataCell(Text('C' * (15 - (index + 5) % 10))),
+                DataCell(Text('D' * (15 - (index + 10) % 10))),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+                DataCell(Text(((index + 0.1) * 25.4).toString())),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
